ce751f40ca245d0119be3e6d9be03b8f3b75b1b3
xyx <82051300+axin1301@users.noreply.github.com>
2024-09-28 04:02:41 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,10 @@
+import shp2txt
+import mapcompare_GE
+import mapcompare_GE_0001
+import average_statistics
+
+
+shp2txt.main()
+mapcompare_GE.main()
+mapcompare_GE_0001.main()
+average_statistics.main()
